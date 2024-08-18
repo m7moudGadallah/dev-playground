4f01660acd548518d5f1dd8e34f4db9c0f5f78db
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-08-18 05:08:50 +0300

    feat(caching/nodejs-redis): add create tag endpoint

--- caching/nodejs-redis/src/index.js
+++ caching/nodejs-redis/src/index.js
@@ -20,6 +20,26 @@ app.get('/', (_, res) => {
   res.status(200).json({ success: true, message: 'App is up and running' });
 });
 
+// Add tag
+app.post('/tags', async (req, res) => {
+  const { tag } = req.body;
+
+  if (!tag) {
+    res
+      .status(400)
+      .json({ success: false, message: '"tag" field is required' });
+  }
+
+  try {
+    await redisClient.sAdd('tags', tag);
+    res.status(200).json({ success: true, message: `Tag "${tag}" added` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: 'Error adding tag', error });
+  }
+});
+
 // Start the server and connect to Redis
 app.listen(PORT, async () => {
   try {
