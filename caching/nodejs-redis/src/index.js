8dd925667f45bc5b233d083cc742d3fb228e25ce
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-08-18 04:12:19 +0300

    chore(caching/nodejs-redis): init simple nodejs app with redis

---
+++ caching/nodejs-redis/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createClient } = require('redis');
+
+const PORT = process.env.PORT || 3000; // Default port if not specified in .env
+const REDIS_URL = process.env.REDIS_URL;
+
+// Create Express App
+const app = express();
+
+// Create Redis Client
+const redisClient = createClient({
+  url: REDIS_URL,
+});
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Start the server and connect to Redis
+app.listen(PORT, async () => {
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis!');
+  } catch (error) {
+    console.error('Failed to connect to Redis', error);
+  }
+
+  console.log(`App is up and running on port ${PORT} 🚀`);
+});
