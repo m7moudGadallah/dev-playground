fd2ec55ac0e9aa8c2c01d264d7a7f0801e1c133f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-12-14 06:49:20 +0200

    feat: update axiosBreaker to user retryStrategy

--- nodejs/circuit-breaker/gateway/axiosBreaker.js
+++ nodejs/circuit-breaker/gateway/axiosBreaker.js
@@ -15,9 +15,10 @@ const BREAKER_CODES = new Set([
  * @param {import('./circuitBreaker')} breakerOptions
  * @param {import('axios').AxiosRequestConfig} [axiosConfig] Optional axios config (e.g., baseURL, headers).
  * @param {import('axios').AxiosInstance | typeof axios} [axiosInstance=axios]
+ * @param {{ exec: (fn: (signal: AbortSignal) => any) => Promise<any> } | undefined} [retryStrategy] Optional RetryStrategy instance
  * @returns {{ request: Function, breaker: CircuitBreaker } & Record<string, Function>}
  */
-function createAxiosBreaker(breakerOptions, axiosConfig = {}, axiosInstance = axios) {
+function createAxiosBreaker(breakerOptions, axiosConfig = {}, axiosInstance = axios, retryStrategy) {
   const options = { ...breakerOptions };
 
   // Default: don't count axios cancellation as a failure unless caller overrides.
@@ -30,8 +31,22 @@ function createAxiosBreaker(breakerOptions, axiosConfig = {}, axiosInstance = ax
     ? axiosInstance.create(axiosConfig)
     : axiosInstance;
 
-  const request = (config) =>
-    breaker.exec((signal) => client.request({ ...config, signal }));
+  const executeOnce = (config, outerSignal) =>
+    breaker.exec((breakerSignal) => {
+      // Tie breaker aborts and outer aborts to the same axios signal
+      const controller = new AbortController();
+      const forwardAbort = (signal) =>
+        signal?.addEventListener('abort', () => controller.abort(signal.reason), { once: true });
+      forwardAbort(breakerSignal);
+      forwardAbort(outerSignal);
+      return client.request({ ...config, signal: controller.signal });
+    });
+
+  const baseRequest = (config, signal) => executeOnce(config, signal);
+
+  const request = retryStrategy
+    ? (config) => retryStrategy.exec((signal) => baseRequest(config, signal))
+    : (config) => baseRequest(config);
 
   const wrapped = { request, breaker };
 
